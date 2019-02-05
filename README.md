401e6dd54a0983691c32c5a9631603c2ceec5c70
Aleksandr Maniunin <aamanunin@gmail.com>
2019-02-06 00:07:49 +0300

    add brain-even.js, add toturial in README.md, some fix

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[![Maintainability](https://api.codeclimate.com/v1/badges/c3d2cbf07d29c8a53ed2/maintainability)](https://codeclimate.com/github/aamanunin/project-lvl1-s438/maintainability)
+[![Maintainability](https://api.codeclimate.com/v1/badges/c3d2cbf07d29c8a53ed2/maintainability)](https://codeclimate.com/github/aamanunin/project-lvl1-s438/maintainability)[![Test Coverage](https://api.codeclimate.com/v1/badges/c3d2cbf07d29c8a53ed2/test_coverage)](https://codeclimate.com/github/aamanunin/project-lvl1-s438/test_coverage)[![Build Status](https://travis-ci.org/aamanunin/project-lvl1-s438.svg?branch=master)](https://travis-ci.org/aamanunin/project-lvl1-s438)
 
-[![Test Coverage](https://api.codeclimate.com/v1/badges/c3d2cbf07d29c8a53ed2/test_coverage)](https://codeclimate.com/github/aamanunin/project-lvl1-s438/test_coverage)
-
-[![Build Status](https://travis-ci.org/aamanunin/project-lvl1-s438.svg?branch=master)](https://travis-ci.org/aamanunin/project-lvl1-s438)
+[![asciicast](https://asciinema.org/a/c6e81hSP9sfgQe9HLsVvp0o0P.svg)](https://asciinema.org/a/c6e81hSP9sfgQe9HLsVvp0o0P)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import readlineSync from 'readline-sync';
 const greet = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return (name);
 };
 
-export default greet;
+const question = (number) => {
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+  let correctAnswer = '';
+  if (number % 2 === 0) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  return false;
+};
+
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const name = greet();
+  if (question(15) === false) {
+    console.log(`Let's try again, ${name}!`);
+    return;
+  }
+  if (question(6) === false) {
+    console.log(`Let's try again, ${name}!`);
+    return;
+  }
+  if (question(7) === false) {
+    console.log(`Let's try again, ${name}!`);
+    return;
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export { greet, game };
