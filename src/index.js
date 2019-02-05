b0d03d80980329693421d64751d945ce626a5b6a
Aleksandr Maniunin <aamanunin@gmail.com>
2019-02-05 16:57:04 +0300

    rename const actual from name index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greet = () => {
-  const actual = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${actual}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default greet;
