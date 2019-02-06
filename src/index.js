c8f7e4af748c1273793ee5c0f102a39a0a19da6b
Aleksandr Maniunin <aamanunin@gmail.com>
2019-02-07 02:12:20 +0300

    add realisation brain-calc game

--- src/index.js
+++ src/index.js
@@ -1,26 +1,64 @@
 import readlineSync from 'readline-sync';
-// После проверки начал уже переделывать код под следующие задание для модульности
+// начал делать вторую игру точь в точь как первую
+// сразу стало видно что повторяется, а какая логика уникальная
+// почти все переработал
 // Вроде все замечания поправил.
 const roundMax = 3;
 
 const greet = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log();
   return (name);
 };
 
+const minNum = 1;
+const maxNum = 100;
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const expressionEven = () => {
-  const minNum = 1;
-  const maxNum = 100;
-  const expression = getRandomInt(minNum, maxNum);
-  return expression;
-};
-
 const isEven = num => (num % 2 === 0);
 
-const questionEven = (number) => {
+const getRandomOperand = () => {
+  let operand;
+  const randomSwitch = getRandomInt(1, 3);
+  switch (randomSwitch) {
+    case 1: operand = '+';
+      break;
+    case 2: operand = '-';
+      break;
+    case 3: operand = '*';
+      break;
+    default: break;
+  }
+  return operand;
+};
+
+// этот синтаксис в инете откапал, это дальше в теории будет?
+const mathItUp = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+};
+
+const questionCalc = () => {
+  const x = getRandomInt(minNum, maxNum);
+  const y = getRandomInt(minNum, maxNum);
+  const operand = getRandomOperand();
+  console.log(operand);
+  console.log(`Question: ${x} ${operand} ${y}`);
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = mathItUp[operand](x, y);
+  console.log(typeof answer);
+  console.log(typeof correctAnswer);
+  if (Number(answer) === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  return false;
+};
+
+const questionEven = () => {
+  const number = getRandomInt(minNum, maxNum);
   console.log(`Question: ${number}`);
   const answer = readlineSync.question('Your answer: ');
   const correctAnswer = isEven(number) ? 'yes' : 'no';
@@ -32,10 +70,10 @@ const questionEven = (number) => {
   return false;
 };
 
-const game = (question, expression) => {
+const game = (question) => {
   const name = greet();
   for (let round = 1; round <= roundMax; round += 1) {
-    const resultCurrentQuestion = question(expression());
+    const resultCurrentQuestion = question();
     if (!resultCurrentQuestion) {
       console.log(`Let's try again, ${name}!`);
       return;
@@ -47,7 +85,15 @@ const game = (question, expression) => {
 const gameEven = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".');
-  game(questionEven, expressionEven);
+  console.log();
+  game(questionEven);
 };
 
-export { greet, gameEven };
+const gameCalc = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('What is the result of the expression?');
+  console.log();
+  game(questionCalc);
+};
+
+export { greet, gameEven, gameCalc };
