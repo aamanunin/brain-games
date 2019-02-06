65b7d13b11824fee192b756f5c776a2eaa1daabc
Aleksandr Maniunin <aamanunin@gmail.com>
2019-02-06 20:16:07 +0300

    fix index.js

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
 
-import { congrulate, greet, gameIsEven } from '..';
+import { gameCalc } from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".');
-const name = greet();
-const game1 = gameIsEven(15);
-const game2 = gameIsEven(6);
-const game3 = gameIsEven(7);
-congrulate(game1, game2, game3, name);
+gameCalc();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
-
-const welcome = () => console.log('Welcome to the Brain Games!');
-
-const descriptionEven = () => console.log('Answer "yes" if number even otherwise answer "no".');
+// После проверки начал уже переделывать код под следующие задание для модульности
+// Вроде все замечания поправил.
+const roundMax = 3;
 
 const greet = () => {
   const name = readlineSync.question('May I have your name? ');
@@ -10,16 +9,21 @@ const greet = () => {
   return (name);
 };
 
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const expressionEven = () => {
+  const minNum = 1;
+  const maxNum = 100;
+  const expression = getRandomInt(minNum, maxNum);
+  return expression;
+};
+
+const isEven = num => (num % 2 === 0);
+
 const questionEven = (number) => {
   console.log(`Question: ${number}`);
   const answer = readlineSync.question('Your answer: ');
-  const isEven = num => (num % 2 === 0);
-  let correctAnswer = '';
-  if (isEven(number)) {
-    correctAnswer = 'yes';
-  } else {
-    correctAnswer = 'no';
-  }
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
   if (answer === correctAnswer) {
     console.log('Correct!');
     return true;
@@ -28,26 +32,22 @@ const questionEven = (number) => {
   return false;
 };
 
-const gameEven = () => {
-  welcome();
-  descriptionEven();
+const game = (question, expression) => {
   const name = greet();
-  let round = 1;
-  const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-  const minNum = 1;
-  const maxNum = 100;
-  for (round; round <= 3; round += 1) {
-    if (questionEven(getRandomInt(minNum, maxNum))) {
-      if (round === 3) console.log(`Congratulations, ${name}!`);
-    } else {
+  for (let round = 1; round <= roundMax; round += 1) {
+    const resultCurrentQuestion = question(expression());
+    if (!resultCurrentQuestion) {
       console.log(`Let's try again, ${name}!`);
       return;
     }
   }
+  console.log(`Congratulations, ${name}!`);
 };
 
-const gameCalc = () => {
-
+const gameEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  game(questionEven, expressionEven);
 };
 
-export { greet, gameEven, gameCalc };
+export { greet, gameEven };
