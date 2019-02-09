71e2c093aee48ae87b1cc296061d375427d9feaa
Aleksandr Maniunin <aamanunin@gmail.com>
2019-02-09 19:11:21 +0300

    fix game-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,11 +4,12 @@ import getRandomInt from '../utils';
 const minNum = -10;
 const maxNum = 10;
 const isPrime = (num) => {
-  let countDel = 0;
-  for (let i = 1; i <= num; i += 1) {
-    if (num % i === 0) countDel += 1;
+  if (num <= 1) return false;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
   }
-  return countDel === 2;
+
+  return true;
 };
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
